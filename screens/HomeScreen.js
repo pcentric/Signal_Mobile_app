126c00007c41e97e7ab0ab17bea7da438b3aa94e
pcentric <priyanshu.s1619@gmail.com>
2021-05-20 13:06:57 +0530

    Second Commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -81,46 +81,8 @@ const HomeScreen = ({ navigation }) => {
             marginRight: 20,
           }}
         >
-          <TouchableOpacity activeOpacity={0.5} onPress={takePicture}>
-            <View style={styles.container}>
-              <RNCamera
-                ref={(ref) => {
-                  this.camera = ref;
-                }}
-                style={styles.preview}
-                type={RNCamera.Constants.Type.back}
-                flashMode={RNCamera.Constants.FlashMode.on}
-                androidCameraPermissionOptions={{
-                  title: "Permission to use camera",
-                  message: "We need your permission to use your camera",
-                  buttonPositive: "Ok",
-                  buttonNegative: "Cancel",
-                }}
-                androidRecordAudioPermissionOptions={{
-                  title: "Permission to use audio recording",
-                  message: "We need your permission to use your audio",
-                  buttonPositive: "Ok",
-                  buttonNegative: "Cancel",
-                }}
-                onGoogleVisionBarcodesDetected={({ barcodes }) => {
-                  console.log(barcodes);
-                }}
-              />
-              <View
-                style={{
-                  flex: 0,
-                  flexDirection: "row",
-                  justifyContent: "center",
-                }}
-              >
-                <TouchableOpacity
-                  // onPress={this.takePicture.bind(this)}
-                  style={styles.capture}
-                >
-                  <Text style={{ fontSize: 14 }}> SNAP </Text>
-                </TouchableOpacity>
-              </View>
-            </View>
+          <TouchableOpacity activeOpacity={0.5}>
+            <AntDesign name="camera" size={24} color="white" />
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => navigation.navigate("AddChat")}
